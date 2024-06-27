ad8713dd36b21a90286f18dc9616b9aa4805b436
lucienheitz <lucienheitz@msn.com>
2024-06-27 18:22:01 +0200

    Randwom walk algorithm

---
+++ randomwalks/graph_recommender.py
@@ -0,0 +1,104 @@
+from __future__ import print_function
+import numpy as np
+from math import log
+
+class GraphRec(object):
+
+    def __init__(self, train_matrix):
+
+        self.train_matrix = train_matrix
+
+        block1 = np.concatenate((np.zeros((self.train_matrix.shape[0],
+                                           self.train_matrix.shape[0])),
+                                 self.train_matrix), axis = 1)
+
+        block2 = np.concatenate((self.train_matrix.T,
+                                 np.zeros((self.train_matrix.shape[1],
+                                           self.train_matrix.shape[1]))), axis = 1)
+
+        self.A = np.concatenate((block1, block2), axis=0)
+        self.D = np.sum(self.A, 1)
+        self.D[self.D == 0] = 0.0001
+
+        self.P = 1.0 * self.A / self.D[:, None]
+        self.P2 = None
+        self.P3 = None
+
+        self.num_u = train_matrix.shape[0]
+        self.num_i = train_matrix.shape[1]
+
+    def compute_p2_p3(self):
+        self.P2 = np.dot(self.P, self.P)
+        self.P3 = np.dot(self.P2, self.P)
+
+    def _user_item_distance_broadening(self, user_pos, item_pos):
+        diff = item_pos - user_pos
+        return np.abs(diff / (self.max_pos - self.min_pos))
+
+    def _user_item_distance_moderating(self, user_pos, item_pos):
+        diff = item_pos - user_pos
+
+        if (user_pos < 0 and diff <= 0):
+            return 0.1
+
+        elif ((user_pos > 0 and diff >= 0)):
+            return 0.1
+
+        else:
+            abs_diff = np.abs(diff/(self.max_pos - self.min_pos))
+            return abs_diff
+
+    def _construct_item_user_distance_matrix(self, u_pos, i_pos, type):
+
+        if (type == "moderating"):
+            i_u_dist = np.abs(np.vectorize(self._user_item_distance_moderating)
+                              (*np.meshgrid(i_pos, u_pos, indexing = 'xy')))
+
+        elif (type == "broadening"):
+            i_u_dist = np.abs(np.vectorize(self._user_item_distance_broadening)
+                              (*np.meshgrid(i_pos, u_pos, indexing = 'xy')))
+
+        upper_block = np.concatenate((np.zeros((self.num_u, self.num_u)), i_u_dist), axis = 1)
+
+        lower_block = np.concatenate((np.zeros((self.num_i, self.num_u)),
+                                      np.zeros((self.num_i, self.num_i))), axis = 1)
+
+        item_user_dist_matrix = np.concatenate((upper_block, lower_block), axis = 0)
+
+        return item_user_dist_matrix
+
+    def train_RWE_ideology(self, u_pos, i_pos, b = 2, iters = 5, type = "moderating"):
+
+        assert len(u_pos) == self.num_u
+        assert len(i_pos) == self.num_i
+
+        self.max_pos = max(np.max(i_pos), np.max(u_pos))
+        self.min_pos = min(np.min(i_pos), np.min(u_pos))
+
+        total_mass = np.zeros((self.P.shape[0], self.P.shape[1]))
+
+        if (self.P2 is None or self.P3 is None):
+            self.compute_p2_p3()
+
+        item_user_distance_matrix = self._construct_item_user_distance_matrix(u_pos, i_pos, type)
+
+        dist_reweigh = item_user_distance_matrix ** b
+
+        P3 = self.P3.copy()
+
+        for iter in range(0, iters):
+
+            erase = P3 - (P3 * dist_reweigh)
+
+            mass = P3 * dist_reweigh
+            D_mod = np.sum(erase, 1)
+
+            v_s = np.diag(D_mod)
+            total_mass += P3
+            total_mass -= erase
+
+            P3 = np.dot(v_s, self.P3)
+
+        recs = total_mass + erase
+
+        return recs[:self.train_matrix.shape[0], self.train_matrix.shape[0]:]
---
+++ randomwalks/positions_from_shares.py
@@ -0,0 +1,139 @@
+import logging
+import numpy as np
+
+# Bibek's code uses sessions (which are no longer part of TF2)
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+
+tf.disable_v2_behavior()
+
+# This is only a problem on the server under Linux. I excluded it for now...
+# Import and set environment level to prevent AVX2 errors
+#import os
+#os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+# "True" skip adjustment below threshold and "False" goes through all score update iterations
+IS_THRESHOLD_ACTIVATED = False
+
+# Skip adjustment of article scores if updates are smaller than the threshold value
+SCORING_THRESHOLD = 0.25
+
+np.random.seed(0)
+np.random.seed(0)
+
+logger = logging.getLogger(__name__)
+
+class IdeologyFromTweets(object):
+
+    def __init__(self, 
+                 matrix, 
+                 user_positions,
+                 rank = 1, lr = 0.05,
+                 regularizer = 0.05,
+                 do_normalize_matrix = False):
+
+        logger.info("Logging Test")
+
+        self.rank = rank
+        self.lr = lr
+        self.regularizer = regularizer
+        self.steps = 1
+        self.sess = tf.Session()
+
+        assert matrix is not None
+        
+        if (do_normalize_matrix):
+            self.ratings_matrix = self.normalize_matrices(matrix, "Ratings Matrix")
+        
+        else:
+            self.ratings_matrix = matrix
+
+        self.shape_ratings = self.ratings_matrix.shape
+
+        assert (user_positions.shape[0] == self.ratings_matrix.shape[0])
+        self.U = tf.convert_to_tensor(tf.reshape(user_positions, [self.shape_ratings[0], self.rank]))
+
+    def normalize_matrices(self, matrix_ip, log_msg = ""):
+
+        matrix = np.nan_to_num(matrix_ip)
+
+        num_nz = np.count_nonzero(matrix)
+        num_total_cells = matrix.shape[0] * matrix.shape[1]
+        sum_nz = np.sum(matrix)
+
+        logging.info("Normalization matrix %s: cells: %d, nz: %d, z: %d, sum: %d, density: %.2f, z/nz: %.2f"
+                     % (log_msg, num_total_cells, num_nz, num_total_cells - num_nz, sum_nz,
+                     1.0 * num_nz / num_total_cells, 1.0 * (num_total_cells - num_nz) / sum_nz))
+
+        return (1.0 * (num_total_cells - num_nz) / sum_nz) * matrix
+
+    def initialize_variables(self):
+
+        self.V = tf.Variable(tf.random_uniform([self.rank, self.shape_ratings[1]], -1.0, 1.0))
+
+        self.b_V = tf.Variable(tf.reshape(tf.zeros([self.shape_ratings[1]]), [1, self.shape_ratings[1]]))
+        self.b_U = tf.Variable(tf.reshape(tf.zeros([self.shape_ratings[0]]), [self.shape_ratings[0], 1]))
+
+        self.UV = tf.matmul(self.U, self.V) + self.b_V + self.b_U
+        self.UV_exp = 1 / (1 + tf.exp(-self.UV))
+
+        self.error = tf.reduce_sum(self.ratings_matrix * tf.log(self.UV_exp) + tf.log(1 - self.UV_exp))
+        self.regularization = self.regularizer * tf.nn.l2_loss(self.U) + \
+                              self.regularizer * tf.nn.l2_loss(self.V)
+
+        self.loss = -(self.error + self.regularization)
+
+        self.optimizer_U = tf.train.AdagradOptimizer(self.lr).minimize(
+            self.loss, var_list=[self.b_U])
+        self.optimizer_V = tf.train.AdagradOptimizer(self.lr).minimize(
+            self.loss, var_list=[self.V, self.b_V])
+
+        self.learnt_V = None
+        self.learnt_U = None
+        self.learnt_bU = None
+        self.learnt_bV = None
+
+        self.init = tf.global_variables_initializer()
+
+    def train(self, steps, print_every_n_steps = 200):
+
+        self.steps = steps
+
+        with self.sess as sess:
+
+            sess.run(self.init)
+
+            lastRun = 100
+            isActive = IS_THRESHOLD_ACTIVATED
+            threshold = SCORING_THRESHOLD
+
+            for i in range(self.steps):
+
+                sess.run(self.optimizer_U)
+                sess.run(self.optimizer_V)
+
+                # Skip further training if the improvements over the iterations is lower than the threshold
+                if (i % print_every_n_steps == 0):
+
+                    #print("%d. Cost: %f" % (i, sess.run(self.loss)))
+                    if (lastRun - sess.run(self.loss) < threshold) and (isActive == True):
+                        break
+                    
+                    lastRun = sess.run(self.loss)
+
+            self.learnt_V = sess.run(self.V)
+            self.learnt_U = sess.run(self.U)
+            self.learnt_bU = sess.run(self.b_U)
+            self.learnt_bV = sess.run(self.b_V)
+
+    def getU(self):
+        return self.learnt_U
+
+    def getV(self):
+        return self.learnt_V
+
+    def getb_u(self):
+        return self.learnt_bU
+
+    def getb_v(self):
+        return self.learnt_bV
